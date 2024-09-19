f782043cdc848bb63cc7c68166e91d0e77eea4c4
ranger <1648186705@qq.com>
2024-09-19 11:23:04 +0800

    feat:post & user 数据结构与模型

---
+++ app/model/PostStatisticsModel.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace app\model;
+
+use support\Model;
+
+/**
+ *
+ */
+class PostStatisticsModel extends Model
+{
+    /**
+     * The connection name for the model.
+     *
+     * @var string|null
+     */
+    protected $connection = 'mysql';
+    
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'post_statistics';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+    
+    
+}
--- resource/sql/v0/v0.0.1.sql
+++ resource/sql/v0/v0.0.1.sql
@@ -45,3 +45,75 @@ CREATE TABLE v0_tag
     `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
     `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic COMMENT = 'tag';
+
+CREATE TABLE `v0_post`
+(
+    `id`           INT(11)             NOT NULL AUTO_INCREMENT,
+    `type`         INT(11)             NOT NULL COMMENT '文章类型 1-company 2-issue',
+    `status`       TINYINT(4)          NOT NULL COMMENT '文章状态 1-草稿 2-待审核 3-已发布 4-已隐藏 5-已删除 6-已过期',
+    `is_top`       TINYINT(4)          NOT NULL DEFAULT 0 COMMENT '是否置顶',
+    `is_original`  TINYINT(4)          NOT NULL DEFAULT 0 COMMENT '是否原创',
+    `company_id`   INT(11)             NOT NULL DEFAULT 0 COMMENT '公司ID',
+    `title`        VARCHAR(255)        NOT NULL COMMENT '文章标题',
+    `desc`         VARCHAR(255)        NOT NULL COMMENT '文章描述',
+    `content`      TEXT                NOT NULL COMMENT '文章内容',
+    `cover`        VARCHAR(255)        NOT NULL DEFAULT '' COMMENT '文章封面',
+    `author_id`    INT(11)             NOT NULL DEFAULT 0 COMMENT '作者ID',
+    `slug`         VARCHAR(255) UNIQUE NOT NULL COMMENT 'seo url',
+    `original_url` VARCHAR(255)        NOT NULL DEFAULT '' COMMENT '原文链接',
+    `created_at`   DATETIME                     DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`   DATETIME                     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT ='Posts table';
+
+CREATE TABLE `v0_post_statistics`
+(
+    `id`         INT(11) NOT NULL AUTO_INCREMENT,
+    `post_id`    INT(11) NOT NULL,
+    `views`      INT(11)  DEFAULT 0 COMMENT '浏览量',
+    `likes`      INT(11)  DEFAULT 0 COMMENT '点赞数',
+    `comments`   INT(11)  DEFAULT 0 COMMENT '评论数',
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT ='Post statistics table';
+
+CREATE TABLE `v0_post_comment`
+(
+    `id`         INT(11) NOT NULL AUTO_INCREMENT,
+    `post_id`    INT(11) NOT NULL,
+    `is_top`     TINYINT(4) NOT NULL DEFAULT 0 COMMENT '是否置顶 1-是 0-否',
+    `user_id`    INT(11) NOT NULL DEFAULT 0 COMMENT '评论者的用户ID, 0 为未注册用户',
+    `comment_id` INT(11) NOT NULL DEFAULT 0 COMMENT '回复的评论ID, 0 为一级评论',
+    `status`     TINYINT(4) NOT NULL COMMENT '评论状态 1-正常 2-隐藏 3-删除',
+    `content`    TEXT    NOT NULL COMMENT '评论内容',
+    `created_at` DATETIME                                 DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME                                 DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT ='Post Comments table';
+
+CREATE TABLE `v0_user`
+(
+    `id`         INT(11)      NOT NULL AUTO_INCREMENT,
+    `username`   VARCHAR(50)  NOT NULL UNIQUE COMMENT '用户名',
+    `status`     TINYINT(4)   NOT NULL COMMENT '用户状态 1-正常 2-禁用',
+    `email`      VARCHAR(100) NOT NULL UNIQUE COMMENT '邮箱',
+    `head_img`   VARCHAR(255) NOT NULL DEFAULT '' COMMENT '头像',
+    `phone`      VARCHAR(11)  NOT NULL UNIQUE COMMENT '手机号',
+    `password`   VARCHAR(255) NOT NULL COMMENT '密码',
+    `desc`       VARCHAR(255) NOT NULL DEFAULT '' COMMENT '描述',
+    `created_at` DATETIME                                 DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME                                 DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT ='Users table';
+
+
+

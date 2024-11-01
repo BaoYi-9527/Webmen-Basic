ccf0dca4a5b271469da19388f934ae64c853551b
ranger <1648186705@qq.com>
2024-11-01 17:48:04 +0800

    perf(project): user action

--- app/model/user/UserPostActionModel.php
+++ app/model/user/UserPostActionModel.php
@@ -11,4 +11,29 @@ class UserPostActionModel extends Model
 {
     protected $table = 'user_post_action';
 
+    const TYPE_LIKE    = 1;    // 点赞
+    const TYPE_DISLIKE = 2;    // 评论
+    const TYPE_STAR    = 3;    // 收藏
+    const TYPE_WATCH   = 4;    // 关注
+
+    public static function action($userId, $postId, $type = self::TYPE_LIKE)
+    {
+        # like 取消 dislike ; dislike 取消 like
+        if ($type == self::TYPE_LIKE) {
+            self::query()->where('user_id', $userId)->where('post_id', $postId)
+                ->where('type', self::TYPE_DISLIKE)
+                ->delete();
+        } elseif ($type == self::TYPE_DISLIKE) {
+            self::query()->where('user_id', $userId)->where('post_id', $postId)
+                ->where('type', self::TYPE_LIKE)
+                ->delete();
+        }
+
+        return self::query()->updateOrCreate([
+            'user_id' => $userId,
+            'post_id' => $postId,
+            'type'    => $type
+        ]);
+    }
+
 }
